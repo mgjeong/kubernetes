b7bd54d006f8f9350c67bf5ba9a5a0c5296cbd31
Artyom Lukianov <alukiano@redhat.com>
2020-04-20 18:12:11 +0300

    Provide unittest for the single NUMA node policy interface
    Includes unittest for:
    
    - Start
    - Allocate
    - RemoveContainer
    - GetTopologyHints
    
    Signed-off-by: Artyom Lukianov <alukiano@redhat.com>

--- pkg/kubelet/cm/memorymanager/memory_manager_test.go
+++ pkg/kubelet/cm/memorymanager/memory_manager_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package memorymanager
 
 import (
@@ -6,11 +22,13 @@ import (
 	"strings"
 	"testing"
 
+	info "github.com/google/cadvisor/info/v1"
 	"github.com/stretchr/testify/assert"
 
-	info "github.com/google/cadvisor/info/v1"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 const (
@@ -19,6 +37,22 @@ const (
 )
 
 type nodeResources map[v1.ResourceName]resource.Quantity
+
+func getPod(podUID string, containerName string, requirements *v1.ResourceRequirements) *v1.Pod {
+	return &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			UID: types.UID(podUID),
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:      containerName,
+					Resources: *requirements,
+				},
+			},
+		},
+	}
+}
 
 // validatePreReservedMemory
 func TestValidatePreReservedMemory(t *testing.T) {

ef72af6a2b5cfc148fd4ecf7aaa1bf4ec35fb81d
Byonggon Chun <bg.chun@samsung.com>
2020-04-22 23:09:15 +0900

    build path to pass Memory Manager Configuration
    Signed-off-by: Byonggon Chun <bg.chun@samsung.com>

--- pkg/kubelet/cm/container_manager.go
+++ pkg/kubelet/cm/container_manager.go
@@ -19,6 +19,7 @@ package cm
 import (
 	"time"
 
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/util/sets"
 	// TODO: Migrate kubelet to either use its own internal objects or client library.
 	v1 "k8s.io/api/core/v1"
@@ -128,14 +129,15 @@ type NodeConfig struct {
 	KubeletRootDir        string
 	ProtectKernelDefaults bool
 	NodeAllocatableConfig
-	QOSReserved                           map[v1.ResourceName]int64
-	ExperimentalCPUManagerPolicy          string
-	ExperimentalCPUManagerReconcilePeriod time.Duration
-	ExperimentalMemoryManagerPolicy       string
-	ExperimentalPodPidsLimit              int64
-	EnforceCPULimits                      bool
-	CPUCFSQuotaPeriod                     time.Duration
-	ExperimentalTopologyManagerPolicy     string
+	QOSReserved                                 map[v1.ResourceName]int64
+	ExperimentalCPUManagerPolicy                string
+	ExperimentalCPUManagerReconcilePeriod       time.Duration
+	ExperimentalMemoryManagerPolicy             string
+	ExperimentalMemoryManagerPreReservedMemory  map[int]map[v1.ResourceName]resource.Quantity
+	ExperimentalPodPidsLimit                    int64
+	EnforceCPULimits                            bool
+	CPUCFSQuotaPeriod                           time.Duration
+	ExperimentalTopologyManagerPolicy           string
 }
 
 type NodeAllocatableConfig struct {
--- pkg/kubelet/cm/container_manager_linux.go
+++ pkg/kubelet/cm/container_manager_linux.go
@@ -342,6 +342,7 @@ func NewContainerManager(mountUtil mount.Interface, cadvisorInterface cadvisor.I
 			nodeConfig.ExperimentalMemoryManagerPolicy,
 			machineInfo,
 			cm.GetNodeAllocatableReservation(),
+			nodeConfig.ExperimentalMemoryManagerPreReservedMemory,
 			nodeConfig.KubeletRootDir,
 			cm.topologyManager,
 		)

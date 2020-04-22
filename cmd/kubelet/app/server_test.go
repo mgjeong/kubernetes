531dd1990c70772ea86f2dc73c746b14fcdd5383
Krzysztof Wiatrzyk <k.wiatrzyk@samsung.com>
2020-04-22 15:51:44 +0200

    Adding additional tests for server.go file, for parsePreReservedMemoryConfig() function.

--- cmd/kubelet/app/server_test.go
+++ cmd/kubelet/app/server_test.go
@@ -61,3 +61,60 @@ func TestValueOfAllocatableResources(t *testing.T) {
 		}
 	}
 }
+
+func TestValueOfPreReservedMemoryConfig(t *testing.T) {
+	testCases := []struct {
+		config        []map[string]string
+		errorExpected bool
+		name          string
+	}{
+		{
+			config:        []map[string]string{{"numa-node": "0", "memory-type": "memory", "limit": "2Gi"}},
+			errorExpected: false,
+			name:          "Valid resource quantity",
+		},
+		{
+			config:        []map[string]string{{"numa-node": "0", "memory-type": "memory", "limit": "2000m"}, {"numa-node": "1", "memory-type": "memory", "limit": "1Gi"}},
+			errorExpected: false,
+			name:          "Valid resource quantity",
+		},
+		{
+			config:        []map[string]string{{"memory-type": "memory", "limit": "2Gi"}},
+			errorExpected: true,
+			name:          "Missing key",
+		},
+		{
+			config:        []map[string]string{{"numa-node": "one", "memory-type": "memory", "limit": "2Gi"}},
+			errorExpected: true,
+			name:          "Wrong 'numa-node' value",
+		},
+		{
+			config:        []map[string]string{{"numa-node": "0", "memory-type": "not-memory", "limit": "2Gi"}},
+			errorExpected: true,
+			name:          "Wrong 'memory' value",
+		},
+		{
+			config:        []map[string]string{{"numa-node": "0", "memory-type": "memory", "limit": "2Gigs"}},
+			errorExpected: true,
+			name:          "Wrong 'limit' value",
+		},
+		{
+			config:        []map[string]string{{"numa-node": "-1", "memory-type": "memory", "limit": "2Gigs"}},
+			errorExpected: true,
+			name:          "Invalid 'numa-node' number",
+		},
+	}
+
+	for _, test := range testCases {
+		_, err := parsePreReservedMemoryConfig(test.config)
+		if test.errorExpected {
+			if err == nil {
+				t.Errorf("%s: error expected", test.name)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("%s: unexpected error: %v", test.name, err)
+			}
+		}
+	}
+}

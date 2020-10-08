c92bc11102e34c0ddfd6f4498a5b46b51bb27dee
Artyom Lukianov <alukiano@redhat.com>
2020-10-08 18:37:36 +0300

    memory manager: add memory manager flag under kubelet options and kubelet config
    The commit also includes generated files after `make generated_files`.
    
    Signed-off-by: Byonggon Chun <bg.chun@samsung.com>

--- pkg/kubelet/apis/config/zz_generated.deepcopy.go
+++ pkg/kubelet/apis/config/zz_generated.deepcopy.go
@@ -186,6 +186,19 @@ func (in *KubeletConfiguration) DeepCopyInto(out *KubeletConfiguration) {
 		copy(*out, *in)
 	}
 	out.Logging = in.Logging
+	if in.ReservedMemory != nil {
+		in, out := &in.ReservedMemory, &out.ReservedMemory
+		*out = make([]map[string]string, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = make(map[string]string, len(*in))
+				for key, val := range *in {
+					(*out)[key] = val
+				}
+			}
+		}
+	}
 	return
 }
 
